47ccca23ba611c7b972c0c34515e14a4c4125959
David Luna <dflunan@gmail.com>
2018-04-15 16:09:46 -0500

    botton hover all pages

--- contratos/filters.py
+++ contratos/filters.py
@@ -1,12 +1,22 @@
 from .models import Contrato
 import django_filters
 
+
+rangos = ((0,"0 - 100'000.000"),
+		  (1,"100'000.001  -  500'000.000"),
+		  (2,"500'000.001  -  1000'000.000"),
+		  (3,"Mayores  a 1000'000.000"))
+
 class ContratosFilter(django_filters.FilterSet):
     objeto = django_filters.CharFilter(lookup_expr='icontains')
     valor_ejecutado_gt = django_filters.NumberFilter(name='valor_ejecutado', lookup_expr='gt')
     valor_ejecutado_lt = django_filters.NumberFilter(name='valor_ejecutado', lookup_expr='lt')
     fecha_terminacion_gt = django_filters.DateFilter(name='fecha_terminacion', lookup_expr='gt')
     fecha_terminacion_lt = django_filters.DateFilter(name='fecha_terminacion', lookup_expr='lt')
+#    valor =  django_filters.ChoiceFilter(choices=rangos, method='filtro_rangos')
+
+    def filtro_rangos(self, queryset, name,value):
+    	print(name)
 
     class Meta:
         model = Contrato
