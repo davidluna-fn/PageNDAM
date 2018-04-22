f0abfe3a76225d6bc7ad109a748042975f930720
David Luna <dflunan@gmail.com>
2018-04-21 22:27:00 -0500

    banner responsive slides

--- contratos/static/js/main.js
+++ contratos/static/js/main.js
@@ -1,49 +1,37 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-
+  
 function showLightbox() {
-  document.getElementById('over').style.display='block';
-   document.getElementById('fade').style.display='block';
+  document.getElementById('fade').style.display='grid';
+
 }
 function hideLightbox() {
-  document.getElementById('over').style.display='none';
   document.getElementById('fade').style.display='none';
 }
 
 function showLightboxLogin() {
-  document.getElementById('overLogin').style.display='block';
-  document.getElementById('fade').style.display='block';
+  document.getElementById('fade-login').style.display='grid';
 }
 function hideLightboxLogin() {
-  document.getElementById('overLogin').style.display='none';
-  document.getElementById('fade').style.display='none';
+  document.getElementById('fade-login').style.display='none';
 }
-
 function resetform() {
-    document.getElementById("form-busqueda").reset();
+document.getElementById("form-busqueda").reset();
 }
 
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
