a16d0ccc4097c4d72f5ec531db3c9602cd980a86
David Luna <dflunan@gmail.com>
2018-05-05 15:44:55 -0500

    bug responsive header

--- contratos/static/js/main.js
+++ contratos/static/js/main.js
@@ -28,12 +28,15 @@ function plusDivs(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
+  var y = document.getElementsByClassName("mySlides-link");
   if (n > x.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+     x[i].style.display = "none";
+     y[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block";  
+  x[slideIndex-1].style.display = "block";
+  y[slideIndex-1].style.display = "block";  
 }
 
 
