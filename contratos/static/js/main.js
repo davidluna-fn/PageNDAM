7c84d29ea483c18ef1daa0f74151e00e3cb84dc0
David Luna <dflunan@gmail.com>
2018-05-18 09:19:10 -0500

    animation slides banner

--- contratos/static/js/main.js
+++ contratos/static/js/main.js
@@ -27,6 +27,11 @@ function plusDivs(n) {
   showDivs(slideIndex += n);
 }
 
+
+var myIndex = 0;
+showDivs(1);
+
+
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
@@ -38,8 +43,37 @@ function showDivs(n) {
      y[i].style.display = "none";  
   }
   x[slideIndex-1].style.display = "block";
-  y[slideIndex-1].style.display = "block";  
+  y[slideIndex-1].style.display = "block";
 }
+
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var y = document.getElementsByClassName("mySlides-link");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+       y[i].style.display = "none";    
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";
+    y[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 6000);    
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
