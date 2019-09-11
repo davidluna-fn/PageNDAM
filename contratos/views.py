a4749828d5caca3616db7cb805141975da186c28
David Luna <davidluna.fn@gmail.com>
2019-09-11 10:24:07 -0500

    Corregir bug id 0

--- contratos/views.py
+++ contratos/views.py
@@ -97,16 +97,13 @@ def search(request):
         if 'pdf' in request.GET:
             template = get_template('invoice.html')
             for i in context['filter'].qs:
-                i.valor_ejecutado = round((i.valor_ejecutado/vsm[str(i.year)])*vsm['2019'],2)
-                i.valor_en_sm = round(i.valor_ejecutado /vsm['2019'],2)
-                i.valor_en_sm_participacion = i.valor_en_sm * i.porcentaje_participacion
-                i.valor_ejecutado_participacion = round((i.valor_ejecutado * i.porcentaje_participacion),2)
-
-                i.valor_ejecutado = SetMoneda(i.valor_ejecutado,"$",2)
-                i.valor_en_sm = SetMoneda(i.valor_en_sm,"",2)
-                i.valor_en_sm_participacion = SetMoneda(i.valor_en_sm_participacion,"",2)
-                i.valor_ejecutado_participacion = SetMoneda(i.valor_ejecutado_participacion,"$",2)
-
+                try:
+                    i.valor_ejecutado = round((i.valor_ejecutado/vsm[str(i.year)])*vsm['2019'],2)
+                    i.valor_ejecutado_participacion = round((i.valor_ejecutado * i.porcentaje_participacion),2)
+                    i.valor_ejecutado = SetMoneda(i.valor_ejecutado,"$",2)
+                    i.valor_ejecutado_participacion = SetMoneda(i.valor_ejecutado_participacion,"$",2)
+                except:
+                    continue
             
             html = template.render(context)
             pdf = render_to_pdf('invoice.html', context)
