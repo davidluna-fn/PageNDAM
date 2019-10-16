e4ab18af1ce874da260df736dc18ef897a3ebd51
David Luna <davidluna.fn@gmail.com>
2019-10-15 20:02:02 -0500

    Actualizacion Django==2.2 y django-filter==2.2.0

--- contratos/filters.py
+++ contratos/filters.py
@@ -9,10 +9,10 @@ rangos = ((0,"0 - 100'000.000"),
 
 class ContratosFilter(django_filters.FilterSet):
     objeto = django_filters.CharFilter(lookup_expr='icontains')
-    valor_ejecutado_gt = django_filters.NumberFilter(name='valor_ejecutado', lookup_expr='gt')
-    valor_ejecutado_lt = django_filters.NumberFilter(name='valor_ejecutado', lookup_expr='lt')
-    fecha_terminacion_gt = django_filters.DateFilter(name='fecha_terminacion', lookup_expr='gt')
-    fecha_terminacion_lt = django_filters.DateFilter(name='fecha_terminacion', lookup_expr='lt')
+    valor_ejecutado_gt = django_filters.NumberFilter(field_name='valor_ejecutado', lookup_expr='gt')
+    valor_ejecutado_lt = django_filters.NumberFilter(field_name='valor_ejecutado', lookup_expr='lt')
+    fecha_terminacion_gt = django_filters.DateFilter(field_name='fecha_terminacion', lookup_expr='gt')
+    fecha_terminacion_lt = django_filters.DateFilter(field_name='fecha_terminacion', lookup_expr='lt')
 #    valor =  django_filters.ChoiceFilter(choices=rangos, method='filtro_rangos')
 
     def filtro_rangos(self, queryset, name,value):
