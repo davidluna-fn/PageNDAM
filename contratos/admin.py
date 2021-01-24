f6ba8819c334a70956446f5767ff5055a06cca50
David Luna <davidluna.fn@gmail.com>
2021-01-24 10:06:32 -0500

    configurar visualizacion de admin

--- contratos/admin.py
+++ contratos/admin.py
@@ -4,26 +4,26 @@ from .models import Contrato, ValorSM
 # Register your models here.
 
 class AdminContrato(admin.ModelAdmin):
-	fieldsets = [['Identidicadores', {'fields': [('numero','year'),
+	fieldsets = [['Identidicadores', {'fields': ['numero','year',
 												'numero_en_proponente',
-												('departamento','municipio')]}],
+												'departamento','municipio']}],
 				 [u'Descripción',{'fields':['entidad_contratante','empresa',
 				 										 'tipo_de_obra','objeto',
-				 										 ('plazo_en_dias','fecha_terminacion')]}],
-				 [u'Infomración economica',{'fields':[('valor_ejecutado',
-				 									  'porcentaje_participacion')]}],
-				 [u'Archivos', {'fields':[('contrato','acuerdo_consorcial'),
-				 						  ('adicional1','adicional2'),
-				 						  ('acta_recibo_final','acta_liquidacion'),
-				 						  ('certificacion_de_obra','certificacion_director')]}]
+				 										 'plazo_en_dias','fecha_terminacion']}],
+				 [u'Infomración economica',{'fields':['valor_ejecutado',
+				 									  'porcentaje_participacion']}],
+				 [u'Archivos', {'fields':['contrato','acuerdo_consorcial',
+				 						  'adicional1','adicional2',
+				 						  'acta_recibo_final','acta_liquidacion',
+				 						  'certificacion_de_obra','certificacion_director']}]
 				]
 
 
-	list_display  		= ['numero','year','departamento','municipio','objeto']
+	list_display  		= ['numero','year','departamento','municipio','objeto','empresa']
 	list_filter   		= ['year','departamento','municipio','valor_ejecutado','tipo_de_obra']
 	list_display_links 	= ['objeto'] 
 	#list_editable = ['objeto']
-	search_fields = ['year','departamento','municipio','objeto']
+	search_fields = ['year','departamento','municipio','objeto','empresa']
 
 
 	class Meta:
